e86491dabaca21f42f4a828dc743d963cfd02058
Jotaro Oyanagi <mj.creative.lab@gmail.com>
2016-11-28 16:55:36 +0900

    [change]ignore fodler

--- gulpfile.js
+++ gulpfile.js
@@ -6,25 +6,31 @@ const gulp = require( 'gulp' );
 const browser = require( 'browser-sync' );
 const ejs = require( 'gulp-ejs' );
 const gif = require( 'gulp-if' );
+const imgMin = require( 'gulp-imagemin' );
 const notify = require( 'gulp-notify' );
 const plumber = require( 'gulp-plumber' );
+const runSequence = require('run-sequence');
 const sass = require( 'gulp-sass' );
 const sourcemaps = require( 'gulp-sourcemaps' );
 const uglify = require( 'gulp-uglify' )
 const objSrcPaths = {
 		ejs: 'src/ejs/',
+		img: 'src/img/',
 		js: 'src/js/',
 		sass: 'src/scss/'
 	};
 
-gulp.observeFile = ( arrFiles, strPath, func, blnFunc ) => {
+gulp.observeFile = ( strSrcPath, strDistPath, func, blnFunc ) => {
 		gulp
-			.src( arrFiles )
+			.src([
+				strSrcPath + '**/*',
+				'!' + strSrcPath + '**/_*'
+			])
 			.pipe( plumber({
 				errorHandler: notify.onError( '<%= error.message %>' )
 			}))
 			.pipe( gif( blnFunc, func() ) )
-			.pipe( gulp.dest( 'dist' + strPath ) )
+			.pipe( gulp.dest( 'dist' + strDistPath ) )
 			.pipe( browser.reload( {stream: true} ) )
 		;
 	}
@@ -43,40 +49,40 @@ gulp
 	})
 
 	.task( 'ejs', () => {
-		gulp.observeFile([
-			objSrcPaths.ejs + '**/*.ejs',
-			'!' + objSrcPaths.ejs + '**/_*.ejs'
-		], '/', ejs, true);
+		gulp.observeFile(objSrcPaths.ejs, '/', ejs, true);
+	})
+
+	.task( 'img', () => {
+		gulp.observeFile(objSrcPaths.img, '/img/', imgMin, true);
 	})
 
 	.task( 'js', () => {
-		gulp.observeFile([
-			objSrcPaths.js + '**/*.js',
-			'!' + objSrcPaths.js + '**/_*.js'
-		], '/js/', uglify, false);
+		gulp.observeFile(objSrcPaths.js, '/js/', uglify, false);
 	})
 
 	.task( 'sass', () => {
 		gulp
 			.src([
-				objSrcPaths.sass + '**/*.scss',
-				'!' + objSrcPaths.sass + '**/_*.scss'
+				objSrcPaths.sass + '**/*',
+				'!' + objSrcPaths.sass + '**/_*'
 			])
 			.pipe( plumber({
 				errorHandler: notify.onError( '<%= error.message %>' )
 			}))
 			.pipe( sourcemaps.init() )
 			.pipe( sass( {outputStyle: 'expanded'} ) )
-			.pipe( sourcemaps.write( '../_maps' ) )
+			.pipe( sourcemaps.write( '../_map' ) )
 			.pipe( gulp.dest( 'dist/css/' ) )
 			.pipe( browser.reload( {stream: true} ) )
 		;
 	})
 
 	.task( 'default', ['server'], () => {
-		gulp.watch( objSrcPaths.ejs + '**/*.ejs', ['ejs'] );
-		gulp.watch( objSrcPaths.js + '**/*.js', ['js'] );
-		gulp.watch( objSrcPaths.sass + '**/*.scss', ['sass'] );
+		gulp.watch( objSrcPaths.ejs + '**/*', ['ejs'] );
+		gulp.watch( objSrcPaths.img + '**/*', ['img'] );
+		gulp.watch( objSrcPaths.js + '**/*', ['js'] );
+		gulp.watch( objSrcPaths.sass + '**/*', ['sass'] );
+		return runSequence( ['ejs', 'img', 'js', 'sass'] );
 	})
 
 ;
